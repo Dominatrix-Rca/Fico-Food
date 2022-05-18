1288d693a3f9b282e71f2257209af936f92d87fd
Hallcoder <hallcoder25@gmail.com>
2022-05-18 19:04:14 +0200

    navbar component

--- src/pages/profile2.js
+++ src/pages/profile2.js
@@ -1,1 +1,10 @@
-//
+// import React, { Component } from 'react';
+// class Profile2 extends Component {
+//     render() { 
+//         return (
+
+//         );
+//     }
+// }
+ 
+// export default Profile2;
