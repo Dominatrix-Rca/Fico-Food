67a52267f21622dcf38b1329f16e2111f1fd5f36
Isimbilysa <lysaisimbi7@gmail.com>
2022-05-18 21:40:24 +0200

    overview

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import ClientFun from './components/client/clientJoin';
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import OverviewFun from './components/overview/overviewFun';
+import NavBar from './components/navBar';
 
 
 function App() {
   return (
     <>
     <div>
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path='/client' element={<ClientFun />}></Route>
@@ -19,14 +20,7 @@ function App() {
       {/* <Overview/> */}
       </div>
 </>
-=======
-import NavBar from './components/navBar';
-
-function App() {
-  return (
-    <NavBar />
->>>>>>> f46a42f45bdbb44bc975c0df200c70c8b64a03d5
-  );
+  )
 }
 
 export default App;
